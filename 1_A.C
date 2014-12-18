05b3c9ab73daf5f694b67e8e2ac3a9e1e2bd3018
maseera ali <maseeraali@ubuntu.(none)>
2014-12-17 16:21:13 -0800

    code1

---
+++ 1_A.C
@@ -0,0 +1,15 @@
+//to calculate gross salary
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ int basic_sal;
+float gross_sal,a1,a2;
+clrscr();
+ printf("Enter basic salary");
+ scanf("%d",&basic_sal);
+ a1=(4*basic_sal)/10;
+ a2=(2*basic_sal)/10;
+ gross_sal=basic_sal-a1-a2;
+ printf("%f",gross_sal);
+ getch();
+ }
